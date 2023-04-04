fcf2508b08886404c29f3dafa477598dba7d0af7
Rynzok <123980260+Rynzok@users.noreply.github.com>
2023-04-04 22:26:04 +0300

    Магический квадрат. мелкие правки. При вводе фразы количественно не равной квадрату программа не вылеатет при дешифровке, но даёт неверный результат. Шифрование работает. Проверена работа с доком

--- magic_square_shifrovanie.py
+++ magic_square_shifrovanie.py
@@ -1,16 +1,19 @@
-from faind_dels import find_all_dels
+# from faind_dels import find_all_dels
+import math
 
 
 def magic_square_shifr_algoritm(message):
-    size = find_all_dels(int(len(message)))
-    s = size[0]
+    # size = find_all_dels(int(len(message)))
+    # s = size[0]
+    lenght = int(len(message))
+    s = math.ceil(lenght**0.5)
     if s % 2 == 0:
         s += 1
     q = [[0 for j in range(s)] for i in range(s)]
     p = 0
     i = s // 2
     j = 0
-    while p < (s * s):
+    while p < lenght:
         q[i][j] = message[p]
         ti = i + 1
         if ti >= s:
@@ -26,8 +29,8 @@ def magic_square_shifr_algoritm(message):
         p = p + 1
     n = 0
     full_massiv = [''] * int(len(message))
-    for i in range(size[1]):
-        for j in range(size[0]):
+    for i in range(s):
+        for j in range(s):
             if q[i][j] != 0:
                 full_massiv[n] = str(q[i][j])
                 n += 1
@@ -43,8 +46,9 @@ def magic_square_shifr_algoritm(message):
 
 def magic_square_deshifr_algoritm(message):
     lenght = int(len(message))
-    size = find_all_dels(int(len(message)))
-    s = size[0]
+    # size = find_all_dels(int(len(message)))
+    # s = size[0]
+    s = math.ceil(lenght**0.5)
     if s % 2 == 0:
         s += 1
 
@@ -65,16 +69,16 @@ def magic_square_deshifr_algoritm(message):
     p = 0
     i = s // 2
     j = 0
-    while p < (s * s):
+    while p < lenght:
         full_massiv[p] = str(massiv[i][j])
-        massiv[i][j] = 0
+        massiv[i][j] = '0'
         ti = i + 1
         if ti >= s:
             ti = 0
         tj = j - 1
         if tj < 0:
             tj = s - 1
-        if massiv[ti][tj] == 0:
+        if massiv[ti][tj] == '0':
             ti = i
             tj = j + 1
         i = ti
