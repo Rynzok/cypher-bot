39d800695457a9ce323a67824dc542f4d911cfe1
Виктор Киселёв <vkisel102@yandex.ru>
2023-05-07 16:36:15 +0300

    Преобразование двоичного в код Грея работает, но при декодинге  почему-то ломаеться с заглавными бувами

--- code_grey.py
+++ code_grey.py
@@ -5,9 +5,22 @@ def code_grey_shifr_algoritm(message_no_space):
         byte_array = message_no_space[i].encode('cp1251')
         binary_int = int.from_bytes(byte_array, "big")
         binary_string = bin(binary_int)
+
         b_string = [''] * (len(binary_string) - 2)
         for j in range(2, len(binary_string)):
             b_string[j - 2] = str(binary_string[j])
-        full_massiv[i] = "".join(b_string)
+        binary_num = "".join(b_string)
+
+        gray_code = convert_gray(binary_num)
+
+        gray_code = int(gray_code, 10)
+        gray_code = int.to_bytes(gray_code, 3, "big")
+        full_massiv[i] = gray_code.decode('cp1251')
         # full_massiv[i] = str(message_no_space[i].encode('cp1251'))
     return full_massiv
+
+
+def convert_gray(binary):
+    binary = int(binary, 2)
+    binary ^= (binary >> 1)
+    return bin(binary)[2:]
