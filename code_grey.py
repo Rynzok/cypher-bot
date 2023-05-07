b9519f22df5a5a7e29adea5532b998f8868e91fa
Виктор Киселёв <vkisel102@yandex.ru>
2023-05-07 16:57:39 +0300

    Шифрование работает, только не вводите букву п

--- code_grey.py
+++ code_grey.py
@@ -4,17 +4,17 @@ def code_grey_shifr_algoritm(message_no_space):
     for i in range(lenght):
         byte_array = message_no_space[i].encode('cp1251')
         binary_int = int.from_bytes(byte_array, "big")
-        binary_string = bin(binary_int)
+        binary_string = bin(binary_int)[2:]
 
-        b_string = [''] * (len(binary_string) - 2)
-        for j in range(2, len(binary_string)):
-            b_string[j - 2] = str(binary_string[j])
-        binary_num = "".join(b_string)
+        # b_string = [''] * (len(binary_string) - 2)
+        # for j in range(2, len(binary_string)):
+        #     b_string[j - 2] = str(binary_string[j])
+        # binary_num = "".join(b_string)
 
-        gray_code = convert_gray(binary_num)
+        gray_code = convert_gray(binary_string)
 
-        gray_code = int(gray_code, 10)
-        gray_code = int.to_bytes(gray_code, 3, "big")
+        gray_code = int(gray_code, 2)
+        gray_code = gray_code.to_bytes(3, "big")
         full_massiv[i] = gray_code.decode('cp1251')
         # full_massiv[i] = str(message_no_space[i].encode('cp1251'))
     return full_massiv
