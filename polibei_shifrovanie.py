80ff5c6db8bd4fbe77444070d95c8a19fa6dd43f
Виктор Киселёв <vkisel102@yandex.ru>
2023-05-10 20:52:51 +0300

    Реализован 3 метод полибея

--- polibei_shifrovanie.py
+++ polibei_shifrovanie.py
@@ -73,3 +73,47 @@ def polibei_deshirf_algoritm_2(message_no_space):
         text[i] = PolibeiK[full_massiv[i]]
         n += 1
     return text
+
+
+def polibei_shirf_algoritm_3(message_no_space):
+    lenght = int(len(message_no_space))
+    massiv = [[0] * 2] * lenght
+    massiv2 = [0] * (lenght * 2)
+    full_massiv = [''] * lenght
+    text = [''] * lenght
+    for i in range(lenght):
+        massiv[i] = (Polibei[message_no_space[i]])
+    n = 0
+    for i in range(2):
+        for j in range(lenght):
+            massiv2[n] = massiv[j][i]
+            n += 1
+    massiv2.append(massiv2.pop(0))
+    n = 0
+    for i in range(lenght):
+        full_massiv[i] = str(massiv2[n]) + str(massiv2[n+1])
+        text[i] = PolibeiK[full_massiv[i]]
+        n += 2
+    return text
+
+
+def polibei_deshirf_algoritm_3(message_no_space):
+    lenght = int(len(message_no_space))
+    massiv = [[0] * 2] * lenght
+    massiv2 = [0] * (lenght * 2)
+    full_massiv = [''] * lenght
+    text = [''] * lenght
+    for i in range(lenght):
+        massiv[i] = (Polibei[message_no_space[i]])
+    n = 0
+    for i in range(lenght):
+        for j in range(2):
+            massiv2[n] = massiv[i][j]
+            n += 1
+    massiv2.insert(0, massiv2.pop())
+    n = 0
+    for i in range(lenght):
+        full_massiv[i] = str(massiv2[n]) + str(massiv2[n+lenght])
+        text[i] = PolibeiK[full_massiv[i]]
+        n += 1
+    return text
