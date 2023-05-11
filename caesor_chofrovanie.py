799a5fe118ee80567c346112af6bf4a3ae026d22
Виктор Киселёв <vkisel102@yandex.ru>
2023-05-11 12:41:46 +0300

    Немного сократил код в шифре Цезаря, но проблема с минусом осталась. Её корень в представление числового ключа как массива, и прочих манипуляций при get_n_key

--- caesor_chofrovanie.py
+++ caesor_chofrovanie.py
@@ -3,9 +3,7 @@ from dictionary import simple_dictionary
 
 def caesar_shifr_algoritm(message_no_space, step_m):
     lenght = int(len(message_no_space))
-    step = 0
-    for i in range(len(step_m)):
-        step += int(step_m[i]) * 10**(len(step_m) - i - 1)
+    step = int(''.join((str(i) for i in step_m)))
     full_massiv = [0] * lenght
     text = [''] * lenght
     for i in range(lenght):
@@ -22,9 +20,7 @@ def caesar_shifr_algoritm(message_no_space, step_m):
 
 def caesar_deshifr_algoritm(message_no_space, step_m):
     lenght = int(len(message_no_space))
-    step = 0
-    for i in range(len(step_m)):
-        step += int(step_m[i]) * 10**(len(step_m) - i - 1)
+    step = int(''.join((str(i) for i in step_m)))
     full_massiv = [0] * lenght
     text = [''] * lenght
     for i in range(lenght):
